35eec2a9a71b5db31f0d99f2caf7743474f4145d
lenagermi <69957145+lenagermi@users.noreply.github.com>
2020-08-27 16:30:07 +0200

    four player table activity layout changes

--- app/src/main/java/com/example/kniffel/MainActivity.java
+++ app/src/main/java/com/example/kniffel/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_table_four_player);
+        setContentView(R.layout.actvitiy_table_three_player);
     }
 }
