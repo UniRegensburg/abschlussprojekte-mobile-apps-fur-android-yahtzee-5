3bc05e0f65e74ef768dafdb3794072e7985cb884
lenagermi <69957145+lenagermi@users.noreply.github.com>
2020-09-01 17:31:13 +0200

    Layout und Activit roll dice

--- app/src/main/java/com/example/kniffel/RollTheDice/RollTheDiceActivity.java
+++ app/src/main/java/com/example/kniffel/RollTheDice/RollTheDiceActivity.java
@@ -1,6 +1,7 @@
 package com.example.kniffel.RollTheDice;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -11,7 +12,6 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.kniffel.InsertResults.TableActivity;
-import com.example.kniffel.MainActivity;
 import com.example.kniffel.R;
 import com.example.kniffel.RollTheDice.ShakeSensor.ShakeSensor;
 import com.example.kniffel.RollTheDice.ShakeSensor.ShakeSensorListener;
@@ -37,13 +37,56 @@ public class RollTheDiceActivity extends AppCompatActivity implements ShakeSenso
         super.onCreate(savedInstanceState);
         initUi();
         initSensor();
-        setupDiceArrays();
+        setupDices();
     }
 
 
-    private void setupDiceArrays() {
+    private void setupDices() {
         diceDrawablePath = new int[]{R.drawable.dice_throw_1, R.drawable.dice_throw_2, R.drawable.dice_throw_3, R.drawable.dice_throw_4, R.drawable.dice_throw_5, R.drawable.dice_throw_6};
         diceEyeNumber = new int[]{1, 2, 3, 4, 5};
+        lockDices();
+    }
+
+    // locks the dices --> numbers aren´t changing, when dice is locked
+    private void lockDices() {
+
+        diceOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                diceOne.setClickable(false);
+                diceOne.setBackgroundResource(R.drawable.custom_button3);
+            }
+        });
+        diceTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                diceTwo.setClickable(false);
+                diceTwo.setBackgroundResource(R.drawable.custom_button3);
+            }
+        });
+        diceThree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                diceThree.setClickable(false);
+                diceThree.setBackgroundResource(R.drawable.custom_button3);
+            }
+        });
+
+        diceFour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               diceFour.setClickable(false);
+                diceFour.setBackgroundResource(R.drawable.custom_button3);
+            }
+        });
+
+        diceFive.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               diceFive.setClickable(false);
+               diceFive.setBackgroundResource(R.drawable.custom_button3);
+            }
+        });
     }
 
     private void initSensor() {
@@ -81,17 +124,42 @@ public class RollTheDiceActivity extends AppCompatActivity implements ShakeSenso
     }
 
     private void shakeDices() {
-        diceOne.setImageResource(diceDrawablePath[diceEyeNumber[0] = randomIndex()]);
-        diceEyeNumber[0]++;
-        diceTwo.setImageResource(diceDrawablePath[diceEyeNumber[1] = randomIndex()]);
-        diceEyeNumber[1]++;
-        diceThree.setImageResource(diceDrawablePath[diceEyeNumber[2] = randomIndex()]);
-        diceEyeNumber[2]++;
-        diceFour.setImageResource(diceDrawablePath[diceEyeNumber[3] = randomIndex()]);
-        diceEyeNumber[3]++;
-        diceFive.setImageResource(diceDrawablePath[diceEyeNumber[4] = randomIndex()]);
-        diceEyeNumber[4]++;
+        if (diceOne.isClickable()) {
+            diceOne.setImageResource(diceDrawablePath[diceEyeNumber[0] = randomIndex()]);
+            diceEyeNumber[0]++;
+        }
+        if (diceTwo.isClickable()) {
+            diceTwo.setImageResource(diceDrawablePath[diceEyeNumber[1] = randomIndex()]);
+            diceEyeNumber[1]++;
+        }
+        if (diceThree.isClickable()) {
+            diceThree.setImageResource(diceDrawablePath[diceEyeNumber[2] = randomIndex()]);
+            diceEyeNumber[2]++;
+        }
+        if (diceFour.isClickable()) {
+            diceFour.setImageResource(diceDrawablePath[diceEyeNumber[3] = randomIndex()]);
+            diceEyeNumber[3]++;
+        }
+        if (diceFive.isClickable()) {
+            diceFive.setImageResource(diceDrawablePath[diceEyeNumber[4] = randomIndex()]);
+            diceEyeNumber[4]++;
+        }
+        unlockDices();
+
     }
+
+    private void unlockDices() {
+        diceOne.setClickable(true);
+        diceOne.setBackgroundColor(Color.TRANSPARENT);
+        diceTwo.setClickable(true);
+        diceOne.setBackgroundColor(Color.TRANSPARENT);
+        diceThree.setClickable(true);
+        diceOne.setBackgroundColor(Color.TRANSPARENT);
+        diceFour.setClickable(true);
+        diceOne.setBackgroundColor(Color.TRANSPARENT);
+        diceFive.setClickable(true);
+    }
+
 
     private int randomIndex() {
         Random random = new Random();
